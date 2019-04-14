c00877667e4f4cc38baec475bb6f50ddd3ec089a
yoLUkAsss <sandoval.lucasj@gmail.com>
2019-04-14 15:03:03 -0300

    Add new project structure. Right now we are using create-react-app

--- test/actions/ticker.spec.js
+++
@@ -1,7 +0,0 @@
-import { nextTick } from 'actions/ticker';
-
-describe('Ticker Action', () => {
-  it('nextTick should return an action', () => {
-    expect(nextTick()).toMatchSnapshot();
-  });
-});
