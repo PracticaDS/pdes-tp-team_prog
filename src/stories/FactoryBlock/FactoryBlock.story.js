8d157672b1ddf52c2f3a6d28763d812052f53abd
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-04-18 17:50:15 -0300

    adding storybook

---
+++ src/stories/FactoryBlock/FactoryBlock.story.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Block from '../../components/FactoryBlocks/Block/Block'
+
+storiesOf('FactoryBlock', module).add('Simple EmptyBlock', () => (
+  <Block position={{ row: 1, column: 1 }} />
+))
