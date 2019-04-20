a61958c8b6d177dda0b9dac45d3564a6a588722c
Federico Lochbaum <federico.lochbaum@gmail.com>
2019-04-20 13:04:44 -0300

    Resources navbar (#34)
    * adding resouces Bar
    
    * fixing erro

---
+++ src/components/Resources/ResourcesBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import { Constant } from './ResourcesBar.constants'
+import './ResourcesBar.css'
+
+// eslint-disable-next-line react/prop-types
+const ResourcesBar = ({ currency }) => (
+  <div className="ResourcesBar">
+    <AppBar color="default">
+      <Toolbar variant="dense">
+        <Typography variant="h6" color="inherit">
+          {`${`${Constant.currency}: ${currency}`}`}
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+)
+
+export default ResourcesBar
