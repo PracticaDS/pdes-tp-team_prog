1a94d6e2343f3264ebbd593c5757738195fd844f
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-04-21 13:23:08 -0300

    using jest.fn()

--- src/components/Game/Game.spec.jsx
+++ src/components/Game/Game.spec.jsx
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer'
 import Game from './Game'
 
 it('render simple Game Component', () => {
-  const props = { restartCurrency: () => 2, startGame: () => 1, dimensions: { n: 6, m: 6 } }
+  const props = { restartCurrency: jest.fn(), startGame: jest.fn(), dimensions: { n: 6, m: 6 } }
   const tree = renderer.create(<Game {...props} />)
   expect(tree).toMatchSnapshot()
 })
