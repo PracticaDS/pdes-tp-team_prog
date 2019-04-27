cea36c2c274b458c01020f54ab9eeebb5b26c3da
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-04-27 00:01:42 -0300

    adding style

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../../store/ReduxStore'
 import Game from '../Game/Game'
-import './App.css'
 
 const store = configureStore()
 
