5443aee704ab4d0a93b9770a32e780e4aa1e2e8f
LSExiLio <sandoval.lucasj@gmail.com>
2019-04-28 06:28:24 -0300

    Fix - enabled .jsx extension for stories files

--- src/stories/Game/Game.story.jsx
+++ src/stories/Game/Game.story.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable react/jsx-key */
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
-import FactoryGrid from '../../components/FactoryGrid/FactoryGrid'
+import Game from '../../components/Game/Game'
 
 const state = {
   restartCurrency: () => true,
@@ -12,7 +10,7 @@ const state = {
   tick: () => true,
   Grid: {
     dimensions: { n: 6, m: 6 },
-    gridValues: Array(6).fill(Array(6).fill({ type: 'Crafter' })),
+    gridValues: Array(6).fill(Array(6).fill({ type: 'Empty' })),
   },
   GameState: {
     currency: 1000,
@@ -21,8 +19,9 @@ const state = {
 const mockStore = configureStore()
 const store = mockStore(state)
 
-storiesOf('FactoryGrid', module).add('Simple Grid with 1x2 empty blocks', () => (
+// n columns, m rows
+storiesOf('Game', module).add('Simple Game component', () => (
   <Provider store={store}>
-    <FactoryGrid />
+    <Game restartCurrency={() => 2} startGame={() => 1} dimensions={{ n: 10, m: 8 }} />
   </Provider>
 ))
