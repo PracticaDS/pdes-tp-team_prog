5d90c589b9f5a59fac09df6d94ed960829db2e45
LaimeJesus <laimejesu@gmail.com>
2019-05-01 16:57:37 -0300

    add util method

--- src/utils/directions.js
+++ src/utils/directions.js
@@ -4,3 +4,6 @@ export const applyDirection = (position, direction) => {
   if (direction === 'DOWN') return { ...position, row: position.row + 1 }
   return { ...position, column: position.column - 1 }
 }
+
+export const isPositionValid = ({ row, column }, { n, m }) =>
+  row < n && row >= 0 && column >= 0 && column < m
