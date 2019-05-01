2b083475a67c4012291bc0c041880a1c228a253f
LaimeJesus <laimejesu@gmail.com>
2019-05-01 01:14:42 -0300

    add getTick selector

--- src/selectors/GameState.js
+++ src/selectors/GameState.js
@@ -2,3 +2,4 @@ const gameState = state => state.GameState
 export const getCurrency = state => gameState(state).currency
 export const machineSelected = state => gameState(state).machineSelected
 export const actionSelected = state => gameState(state).actionSelected
+export const getTick = state => gameState(state).tick
