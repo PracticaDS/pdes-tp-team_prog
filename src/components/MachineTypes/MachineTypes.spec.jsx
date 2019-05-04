7e88eda46f088b9265ed1ba8e8d22199df5f1d66
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-05-04 19:21:10 -0300

    adding tests

--- src/components/MachineTypes/MachineTypes.spec.jsx
+++ src/components/MachineTypes/MachineTypes.spec.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import MachineTypes from './MachineTypes'
-import Machine from '../Machine/Machine'
+import TransporterMachine from '../Machine/TransporterMachine/TransporterMachine'
+import FurnaceMachine from '../Machine/FurnaceMachine/FurnaceMachine'
+import CrafterMachine from '../Machine/CrafterMachine/CrafterMachine'
+import SellerMachine from '../Machine/SellerMachine/SellerMachine'
+import StarterMachine from '../Machine/StarterMachine/StarterMachine'
 
 describe('Machine Types tests', () => {
-  describe('Render the Machine Types Component with a list of machines', () => {
-    const machine = {
-      id: 1,
-      name: 'machine',
-      image: 'machine',
-    }
-    const machines = [machine]
-    const renderMachine = jest.fn(ma => <Machine machine={ma} />)
-    const MachineTypesComponent = mount(
-      <MachineTypes machines={machines} renderMachine={renderMachine} />,
-    )
+  describe('Render the Machine Types Component', () => {
+    const machines = [
+      StarterMachine,
+      SellerMachine,
+      CrafterMachine,
+      FurnaceMachine,
+      TransporterMachine,
+    ]
+    const renderMachine = jest.fn(Machine => Machine)
 
-    it('Should match the machines prop', () => {
-      expect(MachineTypesComponent.prop('machines')).toBe(machines)
-    })
+    const props = {
+      renderMachine,
+    }
+
+    const MachineTypesComponent = mount(<MachineTypes {...props} />)
 
     it('should call the renderMachine function with every machine as argument', () => {
-      machines.forEach(ma => {
-        expect(renderMachine).toHaveBeenCalledWith(ma)
+      machines.forEach(Ma => {
+        expect(renderMachine).toHaveBeenCalledWith(Ma)
       })
     })
     it('should contain every machine component rendered', () => {
-      machines.forEach(ma => {
-        expect(MachineTypesComponent.containsMatchingElement(<Machine machine={ma} />)).toBeTruthy()
+      machines.forEach(Ma => {
+        expect(MachineTypesComponent.containsMatchingElement(<Ma />)).toBeTruthy()
       })
     })
   })
