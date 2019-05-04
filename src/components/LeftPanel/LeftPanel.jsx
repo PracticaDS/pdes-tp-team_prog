5a7594d48760bf54a513a98d9a227e2a5ae6a67a
LaimeJesus <laimejesu@gmail.com>
2019-05-04 13:25:34 -0300

    add left edition changes

--- src/components/LeftPanel/LeftPanel.jsx
+++ src/components/LeftPanel/LeftPanel.jsx
@@ -4,13 +4,15 @@ import './LeftPanel.css'
 import MachineTypes from '../MachineTypes/MachineTypes'
 import Edition from '../Edition/Edition'
 import MachineConnected from '../Machine/MachineConnected'
+import EditionOptionConnected from '../EditionOption/EditionOptionConnected'
 
 const renderMachine = machine => <MachineConnected machine={machine} />
+const renderOption = edition => <EditionOptionConnected editionOption={edition} />
 
 const LeftPanel = ({ machineTypes, editionOptions }) => (
   <div className="mainLeftPanel">
     <MachineTypes machines={machineTypes} renderMachine={renderMachine} />
-    <Edition elements={editionOptions} />
+    <Edition options={editionOptions} renderOption={renderOption} />
   </div>
 )
 
