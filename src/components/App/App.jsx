ff2fbe4760c3a1e862e9e40211bea023464057d1
LaimeJesus <laimejesu@gmail.com>
2019-05-05 16:52:47 -0300

    add some changes to still be working with game changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../../store/ReduxStore'
-import Game from '../Game/Game'
+import Game from '../Game/GameConnected'
 
 const store = configureStore()
 
--- src/components/LeftPanel/LeftPanel.jsx
+++ src/components/LeftPanel/LeftPanel.jsx
@@ -5,9 +5,10 @@ import MachineTypes from '../MachineTypes/MachineTypes'
 import Edition from '../Edition/Edition'
 import EditionOptionConnected from '../EditionOption/EditionOptionConnected'
 import renderMachine from '../Machine/MachineConnector'
+import { editionOptions } from '../Game/Game.constants'
 
 const renderOption = edition => <EditionOptionConnected editionOption={edition} />
-const LeftPanel = ({ editionOptions }) => (
+const LeftPanel = () => (
   <div className="mainLeftPanel">
     <MachineTypes renderMachine={renderMachine} />
     <Edition options={editionOptions} renderOption={renderOption} />
