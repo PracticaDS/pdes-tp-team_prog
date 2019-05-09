b7995a097310e55bdb1f95f36b6628e920301ec3
LaimeJesus <laimejesu@gmail.com>
2019-05-08 21:25:26 -0300

    refactor connectors to remove renderFunction added in stateToProps because it generates a awful bug, also removed unused props

--- src/components/FactoryGrid/FactoryGridConnector.js
+++ src/components/FactoryGrid/FactoryGridConnector.js
@@ -1,31 +1,12 @@
-import React from 'react'
 import { connect } from 'react-redux'
 import { selectDimensions } from '../../selectors/Grid'
-import Block from '../FactoryBlock/BlockConnected'
-import { machineByType } from '../../utils/machineUtils'
-
-const renderNode = node => {
-  const MachineNode = machineByType[node.machine.type]
-  return <MachineNode className="MachineNode" node={node} />
-}
 
 const connector = FactoryGrid => {
   const mapStateToProps = state => ({
     dimensions: selectDimensions(state),
-    renderBlock: position => (
-      <Block
-        key={`${position.row}_${position.column}`}
-        position={position}
-        renderNode={renderNode}
-      />
-    ),
   })
-  const mapDispatchToProps = () => ({})
 
-  return connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(FactoryGrid)
+  return connect(mapStateToProps)(FactoryGrid)
 }
 
 export default connector
