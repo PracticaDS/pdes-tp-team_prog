c2826a9a44f5936e138a780113c4c19a459f84a7
LaimeJesus <laimejesu@gmail.com>
2019-06-15 12:13:25 -0300

    type(feat): add a middleware to update current game (refactor, breaking)

--- src/store/Middlewares.js
+++ src/store/Middlewares.js
@@ -1,9 +1,10 @@
-import { TICK } from '../utils/actionTypes'
+import { TICK_BEGIN } from '../utils/actionTypes'
+import { updateGame } from '../actions/GameState'
 
 let stack = []
 
 export const frameStackMiddleware = ({ dispatch }) => next => action => {
-  if (action.type === TICK) {
+  if (action.type === TICK_BEGIN) {
     next(action)
     stack.forEach(ac => {
       dispatch(ac)
@@ -14,9 +15,29 @@ export const frameStackMiddleware = ({ dispatch }) => next => action => {
     if (newAction.body && newAction.body.stack) {
       delete newAction.body.stack
     }
-
     stack.push(action)
   } else {
     next(action)
   }
+}
+
+let counts = 1
+
+export const updateMiddleware = ({ getState, dispatch }) => next => action => {
+  if (action.type === TICK_BEGIN) {
+    const currentState = getState()
+    if (counts % currentState.GameState.updateRate === 0) {
+      const userId = currentState.User.id
+      const game = {
+        id: currentState.GameState.id,
+        currency: currentState.GameState.currency,
+        factory: currentState.Grid,
+      }
+      dispatch(updateGame(userId, game))
+      counts = 0
+    } else {
+      counts++
+    }
+  }
+  next(action)
 }
--- src/store/ReduxStore.js
+++ src/store/ReduxStore.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import * as reducers from './Reducers'
-import { frameStackMiddleware } from './Middlewares'
+import { frameStackMiddleware, updateMiddleware } from './Middlewares'
 
 const createReducer = () =>
   combineReducers({
@@ -13,7 +13,7 @@ const createReducer = () =>
 // Receive the reducers and the initialState (that right now we are not passing)
 // Return an instance of redux store with your reducers, initial state and the middlewares
 export default function configureStore(initialState) {
-  const middlewares = [frameStackMiddleware, thunk]
+  const middlewares = [frameStackMiddleware, updateMiddleware, thunk]
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
